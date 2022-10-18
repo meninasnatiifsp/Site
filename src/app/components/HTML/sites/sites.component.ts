0babd4773cf18ea6456689b2fc7f22d531967eeb
meninasnatiifsp <meninas.ti2@gmail.com>
2022-10-18 17:54:51 -0300

    att site

--- src/app/components/HTML/sites/sites.component.ts
+++ src/app/components/HTML/sites/sites.component.ts
@@ -1,15 +1,82 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-sites',
   templateUrl: './sites.component.html',
-  styleUrls: ['./sites.component.scss']
+  styleUrls: ['./sites.component.scss'],
 })
 export class SitesComponent implements OnInit {
 
-  constructor() { }
+  alunas: any[] = [
+    { 
+      nome: 'Ana Vitória', 
+      site: 'LUA',
+      endS: 'ana vitoria.html'
+    }, 
+    { 
+      nome: 'Ana', 
+      site: 'Zahnuvi',
+      endS: 'ana.html'
+    }, 
+    { 
+      nome: 'Bia Moço', 
+      site: 'Escola de bolos',
+      endS: 'bia-moço.html'
+    }, 
+    { 
+      nome: 'Gabrielle', 
+      site: 'Gabi bolos',
+      endS: 'gabrielle.html'
+    }, 
+    { 
+      nome: 'Melissa', 
+      site: 'Mulheres no poder',
+      endS: 'MELISSA.html'
+    }, 
+    { 
+      nome: 'Bia Neves', 
+      site: '---',
+      endS: 'bia_neves.html'
+    }, 
+    { 
+      nome: 'Gabs', 
+      site: '---',
+      endS: 'gabs.html'
+    }, 
+  ];
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  closeResult!: string;
+  nomeAluna!: string;
+  caminho!: string;
+
+  constructor(private modalService: NgbModal) {}
+
+  open(content: any, nome: string, cam: string) {
+    this.nomeAluna = nome;
+    this.caminho = cam;
+
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
   }
 
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
--- src/app/components/grid-base/grid-base.component.ts
+++ src/app/components/grid-base/grid-base.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-grid-base',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grid-base.component.scss']
 })
 export class GridBaseComponent implements OnInit {
+  
+  ngOnInit(): void {}
 
-  constructor() { }
+  closeResult!: string;
+  nomeAluna!: string;
+  caminho!: string;
+  aula!: string;
 
-  ngOnInit(): void {
+  constructor(private modalService: NgbModal) {}
+
+  open(content: any, _aula: string) {
+    this.aula = _aula;
+
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
   }
 
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
